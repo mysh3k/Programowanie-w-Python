63a8e3252649b14389dc2bf36169ecc49c8a1b31
mysh3k <mateusz.r0@gmail.com>
2023-11-23 19:29:58 +0100

    calendar wasn't used
    i hope it's not a crime

--- lab3/task3.py
+++ lab3/task3.py
@@ -1,9 +1,29 @@
-import datetime
-import calendar
-
-year: int = int(input("Podaj rok: "))
+from datetime import datetime, timedelta
 
 
-for month in range(1, 13):
-    print(calendar.month_name[month])
+def payroll_calendar(year):
+    months = ["January", "February", "March", "April", "May", "June",
+              "July", "August", "September", "October", "November", "December"]
 
+    # List to store the payroll dates
+    payroll_dates = []
+
+    for month in range(1, 13):
+        # Determining the last day of the month
+        if month == 12:
+            last_day = datetime(year, month, 31)
+        else:
+            last_day = datetime(year, month + 1, 1) - timedelta(days=1)
+
+        # Check if the last day of the month is saturday(5) or sunday(6)
+        while last_day.weekday() >= 5:  # Monday=0, Sunday=6
+            last_day -= timedelta(days=1)
+
+        # Adding the date to the list in the appropriate format
+        payroll_dates.append(f"{months[month - 1]}: {last_day.strftime('%d-%m-%Y')}")
+    print(payroll_dates)
+    return payroll_dates
+
+
+# Example usage of the function for the year 2023
+payroll_calendar(year_input := int(input('Podaj rok: ')))
